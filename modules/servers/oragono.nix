bbbd09873373a93f7d8dd85c4b6f158d26953300
Raito Bezarius <masterancpp@gmail.com>
2020-10-18 23:21:09 +0200

    oragono: add init oneshot service

--- modules/servers/oragono.nix
+++ modules/servers/oragono.nix
@@ -9,6 +9,7 @@ in
       enable = mkEnableOption "Enable the Oragono IRC server";
       group = mkOption { };
       user = mkOption { };
+      stateDirectory = mkOption { default = "/var/lib/oragono"; };
       package = mkOption {
         description = "Package used to run Oragono";
         default = pkgs.oragono;
@@ -33,11 +34,25 @@ in
         serviceConfig = {
           Type = "simple";
           User = cfg.user;
+          Group = cfg.group;
           WorkingDirectory = cfg.stateDirectory;
           ExecStart = "${cfg.package}/bin/oragono run --conf ${settingsFile}";
           ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
           Restart = "on-failure";
           LimitNOFILE = "1048576";
+        };
+      };
+      systemd.services.oragono-init = {
+        description = "Oragono IRC server initialization (mkcerts)";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = cfg.user;
+          Group = cfg.group;
+          WorkingDirectory = cfg.stateDirectory;
+          ExecStart = "${cfg.package}/bin/oragono mkcerts --conf ${settingsFile}";
+          RemainAfterExit = false;
         };
       };
     };

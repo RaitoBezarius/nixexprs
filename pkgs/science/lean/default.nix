505755ed8aafefd80a7666b387be5eca0243e5aa
Raito Bezarius <masterancpp@gmail.com>
2021-08-20 23:39:45 +0200

    lean: exclude v3.12.0

--- pkgs/science/lean/default.nix
+++ pkgs/science/lean/default.nix
@@ -11,6 +11,6 @@ let
       };
     };
   };
-  brokenReleases = ["v9.9.9" "we-love-bors"];
+  brokenReleases = ["v9.9.9" "we-love-bors" "v3.12.0"];
 in
   mergeMap mkLeanRelease (lib.filterAttrs (name: _: !builtins.any (broken: name == broken) brokenReleases) leanReleases)

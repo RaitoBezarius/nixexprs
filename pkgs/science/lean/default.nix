610b394514f3ad9041a6e0545113acb5741af01e
Raito Bezarius <masterancpp@gmail.com>
2021-08-25 11:58:25 +0200

    lean: exclude v3.11.0

--- pkgs/science/lean/default.nix
+++ pkgs/science/lean/default.nix
@@ -25,6 +25,7 @@ let
     "v3.16.0"
     "v3.16.1"
     "v3.5.1"
+    "v3.11.0"
   ];
 in
   mergeMap mkLeanRelease (lib.filterAttrs (name: _: !builtins.any (broken: name == broken) brokenReleases) leanReleases)

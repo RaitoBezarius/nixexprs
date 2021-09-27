85cf5fd8e719a2f1398e2b98ca089d11bbb35083
Raito Bezarius <masterancpp@gmail.com>
2021-09-28 00:12:15 +0200

    modules(netbox): inferred → anything

--- modules/web-apps/netbox/default.nix
+++ modules/web-apps/netbox/default.nix
@@ -97,7 +97,7 @@ in
         default = {};
       };
       environment = mkOption {
-        type = types.attrsOf types.inferred;
+        type = with types; attrsOf anything;
         default = {
           CACHE_URL = "redis:///${cfg.redis.cachingDatabase}";
           REDIS_TASKS_URL = "redis:///${cfg.redis.queueDatabase}";

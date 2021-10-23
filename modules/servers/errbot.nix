0471a13ea2a997b92633d14e5924acb40da17fce
Raito Bezarius <masterancpp@gmail.com>
2021-10-24 00:20:59 +0200

    modules(errbot): update user logic

--- modules/servers/errbot.nix
+++ modules/servers/errbot.nix
@@ -26,8 +26,21 @@ in
         default = pkgs.errbot;
         defaultText = "pkgs.errbot";
       };
+      user = mkOption {
+        type = types.str;
+        default = "errbot";
+      };
+      group = mkOption {
+        type = types.str;
+        default = "errbot";
+      };
     };
     config = {
+      users.users.${cfg.user} = {
+        isSystemUser = true;
+        inherit (cfg) group;
+      };
+      users.groups.${cfg.group} = {};
       systemd.services.errbot = {
         description = "Errbot: a chat bot";
         after = [ "network.target" ];

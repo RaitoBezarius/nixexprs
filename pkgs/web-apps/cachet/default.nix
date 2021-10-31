2af35b78f85cc10a4e9f067ec0663cd28eaeea61
Raito Bezarius <masterancpp@gmail.com>
2021-10-31 15:02:30 +0100

    pkgs(cachet): update sha256 to a valid one

--- pkgs/web-apps/cachet/default.nix
+++ pkgs/web-apps/cachet/default.nix
@@ -16,7 +16,7 @@ import ./php-packages.nix {
     owner = "fiveai";
     repo = "cachet";
     rev = "0fbeaa1b5649361ffe64ab9a9d960eccd10186ef";
-    sha256 = "sha256-Hh5lStMBrIo6EYHJ7sXnbyRO783Z1Z/jxxie839Js8g=";
+    sha256 = "sha256-kmxDFIbsRdDKPBkmTozcWU8vPfpjnQV7q4UJkTL76ZA=";
     fetchSubmodules = true;
   };
 }

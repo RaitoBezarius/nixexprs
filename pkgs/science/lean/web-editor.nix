bac7d1c01c8ad5de317ea3d901544dd3922bfbe8
Raito Bezarius <masterancpp@gmail.com>
2021-10-31 02:43:35 +0200

    pkgs(lean): finalize web editor build

--- pkgs/science/lean/web-editor.nix
+++ pkgs/science/lean/web-editor.nix
@@ -1,20 +1,20 @@
-{ npmlock2nix, fetchFromGitHub, lean, library ? lean.coreLibrary }:
-npmlock2nix.build {
-  src = fetchFromGitHub {
-    owner = "RaitoBezarius";
-    repo = "lean-web-editor";
-    rev = "b210fea45fb722beff14991e6137be18262ae865";
-    sha256 = "sha256-z8jlRWmiacexL5XrbAArfmPktGGqvDq3th21S4TNTRw=";
-  };
+{ npmlock2nix, symlinkJoin, fetchFromGitHub, lean, library ? lean.coreLibrary }:
+symlinkJoin {
+  name = "${lean.name}-web-editor-using-${library.name}";
+  paths = [
+    (npmlock2nix.build {
+      src = fetchFromGitHub {
+        owner = "RaitoBezarius";
+        repo = "lean-web-editor";
+        rev = "b210fea45fb722beff14991e6137be18262ae865";
+        sha256 = "sha256-z8jlRWmiacexL5XrbAArfmPktGGqvDq3th21S4TNTRw=";
+      };
 
-  installPhase = ''
-    cp -r ${lean.emscripten} $out/
-    echo "emscripten WASM runtime files copied."
-    cp -r ${library} $out/
-    echo "core library files copied."
-    cp -r dist $out/
-    echo "web editor files copied."
-  '';
-
-  buildCommands = [ "npm run build" ];
+      installPhase = "cp -r dist $out";
+      buildCommands = [ "npm run build" ];
+    })
+    lean.emscripten
+    library
+  ];
 }
+

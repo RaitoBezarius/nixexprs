2f448e07da6090e7d4551a6fc810971e2abd86cb
Raito Bezarius <masterancpp@gmail.com>
2021-10-31 14:39:22 +0100

    ci: add NUR caching

---
+++ pkgs/ci.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+let myPkgs = pkgs.callPackage ./default.nix {};
+in
+  with myPkgs; [
+    myNixops
+    beauties
+    cachet
+    wireguard-vanity-address
+    workingEmscripten
+    zig
+    isso
+    kachpass
+  ]

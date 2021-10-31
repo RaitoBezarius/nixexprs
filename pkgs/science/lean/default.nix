f0a3d956d81794d2e9fc1be9c22c4b4d209bf8b8
Raito Bezarius <masterancpp@gmail.com>
2021-10-31 02:58:11 +0200

    pkgs(lean): pass npmlock2nix

--- pkgs/science/lean/default.nix
+++ pkgs/science/lean/default.nix
@@ -1,11 +1,11 @@
-{ lib, fetchFromGitHub, callPackage, emscripten }:
+{ lib, fetchFromGitHub, callPackage, emscripten, npmlock2nix }:
 let
   mergeMap = f: set: builtins.foldl' (x: acc: x // acc) {} (map (k: f k set.${k}) (builtins.attrNames set));
   leanReleases = lib.importJSON ./releases.json;
   processVersionName = ver: lib.removePrefix "v" (lib.replaceChars ["."] ["_"] ver);
   mkLeanRelease = version: releaseInfo: {
     ${processVersionName version} = callPackage ./generic.nix {
-      inherit version emscripten;
+      inherit version emscripten npmlock2nix;
       leanUtils = callPackage ./utils.nix {};
       src = fetchFromGitHub {
         inherit (releaseInfo) owner repo rev sha256;

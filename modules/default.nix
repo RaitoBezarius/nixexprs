15a9332f72217e6b251518285cce5c639c039bc8
Raito Bezarius <masterancpp@gmail.com>
2022-08-07 22:35:13 +0200

    modules(charybdis): fix up entrypoint

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,6 @@
 {
   beauties = ./web-apps/beauties.nix;
-  charybdis = ./charybdis;
+  charybdis = ./servers/charybdis;
   sourcehut = ./sourcehut;
   etebase = ./servers/etebase-server.nix;
   oragono = ./servers/oragono.nix;

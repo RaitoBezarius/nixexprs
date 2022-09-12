95aa0b1558e84f2b1dc38ab7c9123da8063659b9
Raito Bezarius <masterancpp@gmail.com>
2022-09-12 16:08:24 +0200

    pkgs: remove inspircd

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -62,7 +62,6 @@ self = rec {
     beauties = callPackage ./web-apps/beauties { }; # Personal essential Internet web services.
     infcloud = callPackage ./web-apps/infcloud { }; # CalDAV/CardDAV web client.
     oragono = callPackage ./servers/oragono.nix { };
-    inspircd = callPackage ./servers/inspircd { };
     python3PackagesPlus = callPackage ./python-packages {
       python = pkgs.python3;
       wafHook = callPackage ./development/wafHook {};
